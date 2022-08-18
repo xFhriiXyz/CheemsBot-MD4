123baea3545859858c6b8fbea15bfe09de8324b8
xFhriiXyz <111221563+xFhriiXyz@users.noreply.github.com>
2022-08-18 15:47:50 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -56,10 +56,10 @@ global.packname = "Sticker by" //ur sticker watermark packname
 global.author = "XynnBotzMD" //ur sticker watermark author
 
 //Bot theme media
-global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.thum = fs.readFileSync("./XeonMedia/theme/d85368c8c57b068e62fd47f6755317d6.jpg") //ur thumb pic
 global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
 global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
-global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.thumb = fs.readFileSync("./XeonMedia/theme/d85368c8c57b068e62fd47f6755317d6.jpg") //ur thumb pic
 
 //damtabase
 global.premium = ['6282118868431'] //ur premium numbers
