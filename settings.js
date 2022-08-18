984666759a54c60d1e11319ae03ffc7de5365b9c
xFhriiXyz <111221563+xFhriiXyz@users.noreply.github.com>
2022-08-18 15:35:13 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,8 +22,8 @@ global.APIKeys = {
 global.autoTyping = false //autoketik otomatis di gc (true buat on, false buat off)
 global.autoreadpmngc = true //pembacaan otomatis di pc (true to buat on, false buat off)
 global.autoReadGc = true //pembacaan otomatis di gc (true to on, false to off)
-global.autoRecord = true //perekaman otomatis (true to on, false to off)
-global.available = true //tanda online (true to on, false to off)
+global.autoRecord = false //perekaman otomatis (true to on, false to off)
+global.available = false //tanda online (true to on, false to off)
 
 //semt menu variants
 global.vidmenu = fs.readFileSync("./XeonMedia/theme/Cheems-bot.mp4") //gif and video menu
@@ -37,23 +37,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['6285822347348'] //ur owner number
-global.ownername = "ArullOfc" //ur owner name
-global.ytname = "YT: ARULL OFC" //ur yt chanel name
-global.socialm = "GitHub: Warikrr" //ur github or insta name
-global.location = "Indonesia, Kalimantan Selatan, Kotabaru" //ur location
+global.vcardowner = ['6282118868431'] //ur owner number
+global.ownername = "FhriiXyz" //ur owner name
+global.ytname = "YT : XynnZuu" //ur yt chanel name
+global.socialm = "GitHub: xFhriiXyz" //ur github or insta name
+global.location = "Isekai" //ur location
 
 //bot bomdy 
-global.owner = ['6285822347348']
-global.ownertag = '6285822347348' //ur tag number
-global.botname = 'ArullBotzMD' //ur bot name
+global.owner = ['6282118868431']
+global.ownertag = '6282118868431' //ur tag number
+global.botname = 'XynnBotzMD' //ur bot name
 global.linkz = "https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg" //your theme url which will be displayed on whatsapp
 global.websitex = "https://chat.whatsapp.com/GdXv5LxKfDPHJ4WOH2rNu5" //ur website to be displayed
 global.botscript = 'https://youtu.be/k3QkeVdAMOs' //script link
 global.reactmoji = "🗿" //ur menu react emoji
 global.themeemoji = "😩" //ur theme emoji
 global.packname = "Sticker by" //ur sticker watermark packname
-global.author = "ArullOfc" //ur sticker watermark author
+global.author = "XynnBotzMD" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -62,7 +62,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['6285822347348'] //ur premium numbers
+global.premium = ['6282118868431'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -96,7 +96,7 @@ global.mess = {
     private: 'Fitur Hanya Digunakan Untuk Obrolan Pribadi!',
     bot: 'Fitur Ini Hanya Untuk Bot',
     wait: 'Dalam proses...',
-    linkm: 'Mana linknya?',
+    linkm: 'Mana linknya?🤨📸',
     error: 'Kesalahan!!',
     endLimit: 'Batas Harian Anda Telah Kedaluwarsa, Batas Akan Direset Setiap 12 Jam',
     ban: 'Anda telah diblokir oleh pemiliknya, jika Anda ingin diblokir, chat pemilik.',
@@ -105,7 +105,7 @@ global.mess = {
 }
     global.limitawal = {
     premium: "Infinity",
-    free: 12,
+    free: 75,
     monayawal: 1000
 }
    global.rpg = {
